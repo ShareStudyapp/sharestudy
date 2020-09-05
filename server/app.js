c06352dae363217750c58dfb6717a8bac1242d17
최성준 <csj2702@naver.com>
2020-09-06 02:04:25 +0900

    디비접속성공

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
-
+const db = require('./models')
 const app = express();
 
+db.sequelize.sync()
+    .then(()=>{
+        console.log('db 연결 성공')
+    })
+    .catch(console.error)
 app.get('/',(req,res) => {
     res.send('test')
 })
