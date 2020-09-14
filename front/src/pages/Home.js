6b2b0033df782d116fb336e38534c23923eaf1a6
최성준 <csj2702@naver.com>
2020-09-15 00:14:20 +0900

    피드리스트 이미지 출력

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Feeds from '../components/Feed/Feeds'
 
 function Home() {
     return (
         <div className="home">
-          <h1>Home</h1>  
+          <Feeds />
         </div>
     )
 }
