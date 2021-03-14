c85fda9d65ae2acb989a7230aa90496404fef448
jiyoon lee <wldbs4282@gmail.com>
2021-03-14 23:36:30 +0900

    Add noti page

---
+++ front/src/pages/Noti/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Noti = () => {
+  return (
+    <>
+      <header
+        style={{
+          backgroundColor: 'grey',
+          height: '8vw',
+          display: 'flex',
+          alignItems: 'center',
+          padding: '2vw'
+        }}
+      >
+        <svg
+          width="1.5em"
+          height="1.5em"
+          viewBox="0 0 20 34"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M19.45 3.45L16.5 0.5L0 17L16.5 33.5L19.45 30.55L5.9 17L19.45 3.45Z"
+            fill="black"
+          />
+        </svg>
+        <h2
+          style={{
+            display: 'inline-block',
+            fontSize: '1.5em',
+            margin: 0
+          }}
+        >
+          알림
+        </h2>
+      </header>
+    </>
+  );
+};
+
+export default Noti;
